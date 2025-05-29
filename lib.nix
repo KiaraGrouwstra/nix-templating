385a8277731127dd88113c81338369ba7bf016b5
cinereal <cinereal@riseup.net>
2025-05-29 11:19:33 +0200

    add json templating

--- lib.nix
+++ lib.nix
@@ -1,5 +1,5 @@
-{ pkgs, nix_templater }:
-{
+{ pkgs, lib, nix_templater }:
+rec {
   # placeholder to be substituted with the content of a secret file
   fileContents = file: {
    outPath = "<${builtins.placeholder "nix_template"}${toString file}${builtins.placeholder "nix_template"}>";
@@ -21,4 +21,13 @@
       cp $scriptPath $out/bin/${name}
       chmod +x $out/bin/${name}
     '';
+
+  template_generator = generator: { name, value, outPath }: template_text {
+    inherit name outPath;
+    text = generator value;
+  };
+
+  template_json = options: template_generator (lib.generators.toJSON options);
+  template_yaml = options: template_generator (lib.generators.toYAML options); # just json
+  template_ini = options: template_generator (lib.generators.toINI options);
 }
